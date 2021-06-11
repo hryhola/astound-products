21c725ac0b467a7f03f27cadd42c8f8af1e934af
hryhola <vladgrygola@gmail.com>
2021-06-11 10:59:10 +0300

    redux persist

--- client/store/store.js
+++ client/store/store.js
@@ -1,10 +1,32 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { configureStore, combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import { createWhitelistFilter } from "redux-persist-transform-filter";
+import storage from "redux-persist/es/storage";
+import hardSet from "redux-persist/lib/stateReconciler/hardSet";
 
 import products from "./productsSlice";
 
-const root = combineReducers({ products });
+const rootReducer = combineReducers({ products });
+
+const basketFilter = createWhitelistFilter("products", ["basket"]);
+
+const persistConfig = {
+    key: "root",
+    storage,
+    transforms: [basketFilter],
+    stateReconciler: hardSet,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: root,
+    reducer: persistedReducer,
     devTools: true,
+    middleware: getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: ["persist/PERSIST"],
+        },
+    }),
 });
+
+export const persistor = persistStore(store);
